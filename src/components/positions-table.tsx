7f8905350e3107e57f308f0d4cac0d496d4d0555
Nagy Zétény <nagyzeteny6@gmail.com>
2024-08-11 19:57:58 +0200

    hide columns

--- src/components/positions-table.tsx
+++ src/components/positions-table.tsx
@@ -21,6 +21,13 @@ import {
 } from '~/components/ui/table';
 import { Button } from '~/components/ui/button';
 import { currencyFormatter } from '~/utils/currency-formatter';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+} from '~/components/ui/dropdown-menu';
+import { ChevronDown } from 'lucide-react';
 
 export const PositionsTable: React.FC<{
   data: RouterOutputs['position']['getAllWithHoursWorked'];
@@ -65,6 +72,74 @@ export const PositionsTable: React.FC<{
 
   return (
     <>
+      <div className="flex gap-4 max-sm:flex-col">
+        <div className="flex w-full items-center justify-between gap-2">
+          <div className="flex gap-2">
+            <Button
+              onClick={() => table.firstPage()}
+              disabled={!table.getCanPreviousPage()}
+              size="icon"
+              variant="outline"
+            >
+              {'<<'}
+            </Button>
+            <Button
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+              size="icon"
+              variant="outline"
+            >
+              {'<'}
+            </Button>
+          </div>
+
+          <div>
+            Page <b>{table.getState().pagination.pageIndex + 1}</b> of <b>{table.getPageCount()}</b>
+          </div>
+
+          <div className="flex gap-2">
+            <Button
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+              size="icon"
+              variant="outline"
+            >
+              {'>'}
+            </Button>
+            <Button
+              onClick={() => table.lastPage()}
+              disabled={!table.getCanNextPage()}
+              size="icon"
+              variant="outline"
+            >
+              {'>>'}
+            </Button>
+          </div>
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="flex items-center gap-2 sm:w-fit">
+              Columns <ChevronDown size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => (
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                >
+                  {column.columnDef.header?.toString()}
+                </DropdownMenuCheckboxItem>
+              ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -108,50 +183,6 @@ export const PositionsTable: React.FC<{
           </TableBody>
         )}
       </Table>
-
-      <div className="flex items-center justify-between gap-2">
-        <div className="flex gap-2">
-          <Button
-            onClick={() => table.firstPage()}
-            disabled={!table.getCanPreviousPage()}
-            size="icon"
-            variant="outline"
-          >
-            {'<<'}
-          </Button>
-          <Button
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-            size="icon"
-            variant="outline"
-          >
-            {'<'}
-          </Button>
-        </div>
-
-        <div>
-          Page <b>{table.getState().pagination.pageIndex + 1}</b> of <b>{table.getPageCount()}</b>
-        </div>
-
-        <div className="flex gap-2">
-          <Button
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-            size="icon"
-            variant="outline"
-          >
-            {'>'}
-          </Button>
-          <Button
-            onClick={() => table.lastPage()}
-            disabled={!table.getCanNextPage()}
-            size="icon"
-            variant="outline"
-          >
-            {'>>'}
-          </Button>
-        </div>
-      </div>
     </>
   );
 };
