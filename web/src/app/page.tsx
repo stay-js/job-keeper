37c23b77da7fd4ed932e7b357c84f7ac6196ec3e
Ppanni05 <ppanna2005@gmail.com>
2025-09-18 14:07:38 +0200

    update landing page, add move right arrow, update button text

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import { Button } from '~/components/ui/button';
+import { MoveRight } from 'lucide-react';
 
 const Page: React.FC = () => (
   <main className="grid min-h-screen items-center bg-neutral-950 p-6 text-white">
     <div className="flex flex-col items-center gap-6 text-center">
-      <h1 className="text-5xl font-bold">Üdv a Fizu Appban 💸</h1>
+      <h1 className="text-5xl font-bold">Üdv a Fizu App-ban 💸</h1>
       <p className="text-lg text-neutral-400">Kezeld a munkáidat és pozícióidat egyszerűen.</p>
 
-      <Button asChild className="w-fit">
-        <Link href="/dashboard">Menj a Dashboardra →</Link>
+      <Button asChild className="flex w-fit items-center gap-2">
+        <Link href="/dashboard">
+          <span className="text-black">Irány a Dashboard</span>
+          <MoveRight color="black" />
+        </Link>
       </Button>
     </div>
   </main>
