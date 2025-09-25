9c2cb7294aaf771ff18d6d94f7db7b89f8f7b619
Nagy Zétény <nagyzeteny6@gmail.com>
2025-09-25 10:55:04 +0200

    set clerk theme to dark

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import '~/styles/globals.css';
 import { type Viewport } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import { TRPCReactProvider } from '~/trpc/react';
 
 export const viewport: Viewport = {
@@ -10,7 +11,7 @@ export const viewport: Viewport = {
 };
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ClerkProvider>
+  <ClerkProvider appearance={{ baseTheme: dark }}>
     <html lang="en" className={`${GeistSans.variable} dark`}>
       <body className="bg-neutral-950 text-white">
         <TRPCReactProvider>{children}</TRPCReactProvider>
