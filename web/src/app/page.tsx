c1984420b50aaa7ebb27caa39a20bffe891de26c
Nagy Zétény <nagyzeteny6@gmail.com>
2025-09-25 10:52:04 +0200

    redirect to dashboard when user is signed in

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,21 +1,28 @@
-import { Button } from '~/components/ui/button';
-import { MoveRight } from 'lucide-react';
+import { redirect } from 'next/navigation';
 import { SignInButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { MoveRight } from 'lucide-react';
+import { Button } from '~/components/ui/button';
 
-const Page: React.FC = () => (
-  <main className="grid min-h-screen items-center bg-neutral-950 p-6 text-white">
-    <div className="flex flex-col items-center gap-6 text-center">
-      <h1 className="text-5xl font-bold">Üdv a Fizu App-ban 💸</h1>
-      <p className="text-lg text-neutral-400">Kezeld a munkáidat és pozícióidat egyszerűen.</p>
+const Page: React.FC = async () => {
+  const user = await currentUser();
+  if (user) redirect('/dashboard');
 
-      <SignInButton mode="modal">
-        <Button className="flex w-fit items-center gap-2">
-          <span className="text-black">Irány a Dashboard</span>
-          <MoveRight color="black" />
-        </Button>
-      </SignInButton>
-    </div>
-  </main>
-);
+  return (
+    <main className="grid min-h-screen items-center bg-neutral-950 p-6 text-white">
+      <div className="flex flex-col items-center gap-6 text-center">
+        <h1 className="text-5xl font-bold">Üdv a Fizu App-ban 💸</h1>
+        <p className="text-lg text-neutral-400">Kezeld a munkáidat és pozícióidat egyszerűen.</p>
+
+        <SignInButton mode="modal">
+          <Button className="flex w-fit items-center gap-2">
+            <span className="text-black">Irány a Dashboard</span>
+            <MoveRight color="black" />
+          </Button>
+        </SignInButton>
+      </div>
+    </main>
+  );
+};
 
 export default Page;
