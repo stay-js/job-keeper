134059f5ce42fa5376a1defa2a6a4556a76a996c
K.Papp Benjamin <k.papp.benjamin@gmail.com>
2025-09-25 13:42:31 +0200

    change landing page language

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -18,12 +18,12 @@ const Page: React.FC = async () => {
   return (
     <main className="grid min-h-screen items-center bg-neutral-950 p-6 text-white">
       <div className="flex flex-col items-center gap-6 text-center">
-        <h1 className="text-5xl font-bold">Üdv a Fizu App-ban 💸</h1>
-        <p className="text-lg text-neutral-400">Kezeld a munkáidat és pozícióidat egyszerűen.</p>
+        <h1 className="text-5xl font-bold">Welcome to JobKeeper 💸</h1>
+        <p className="text-lg text-neutral-400">Manage your jobs and positions with ease.</p>
 
         <SignInButton mode="modal">
           <Button className="flex w-fit items-center gap-2">
-            <span className="text-black">Irány a Dashboard</span>
+            <span className="text-black">Go to Dashboard</span>
             <MoveRight color="black" />
           </Button>
         </SignInButton>
