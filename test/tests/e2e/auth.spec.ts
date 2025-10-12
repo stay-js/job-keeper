b0e1f069f652b5d7fa14758ea1aa19deac414a23
Ppanni05 <ppanna2005@gmail.com>
2025-10-12 13:26:20 +0200

    add redirect to "/" after sign in using email and password and ui

--- test/tests/e2e/auth.spec.ts
+++ test/tests/e2e/auth.spec.ts
@@ -1,14 +1,11 @@
-import { test, expect } from "@playwright/test";
-import { clerk, setupClerkTestingToken } from "@clerk/testing/playwright";
+import { test, expect } from '@playwright/test';
+import { clerk, setupClerkTestingToken } from '@clerk/testing/playwright';
 
-test.describe("Authentication", () => {
-  test("should open sign in modal when clicking sign in button", async ({
-    page,
-  }) => {
-    await page.goto("/");
+test.describe('Authentication', () => {
+  test('should open sign in modal when clicking sign in button', async ({ page }) => {
+    await page.goto('/');
 
-    const signInButton = page.getByRole("button", { name: /go to dashboard/i });
-    await signInButton.click();
+    await page.click("button:has-text('Go to Dashboard')");
 
     await expect(page.locator('[role="dialog"]')).toBeVisible();
   });
@@ -18,22 +15,17 @@ test.describe("Authentication", () => {
     baseURL,
     browserName,
   }) => {
-    if (browserName === "webkit" && process.env.TEST_ENV === "prod")
-      test.skip();
+    if (browserName === 'webkit' && process.env.TEST_ENV === 'prod') test.skip();
 
     await setupClerkTestingToken({ page });
 
-    await page.goto("/");
+    await page.goto('/');
 
-    const signInButton = page.getByRole("button", { name: /go to dashboard/i });
-    await signInButton.click();
+    await page.click("button:has-text('Go to Dashboard')");
 
-    await page.fill("input#identifier-field", process.env.E2E_CLERK_USER_EMAIL);
+    await page.fill('input#identifier-field', process.env.E2E_CLERK_USER_EMAIL);
     await page.click('button:has-text("Continue")');
-    await page.fill(
-      "input#password-field",
-      process.env.E2E_CLERK_USER_PASSWORD
-    );
+    await page.fill('input#password-field', process.env.E2E_CLERK_USER_PASSWORD);
     await page.click('button:has-text("Continue")');
 
     await page.waitForURL(`${baseURL}/**`, { timeout: 20_000 });
@@ -46,17 +38,16 @@ test.describe("Authentication", () => {
     baseURL,
     browserName,
   }) => {
-    if (browserName === "webkit" && process.env.TEST_ENV === "prod")
-      test.skip();
+    if (browserName === 'webkit' && process.env.TEST_ENV === 'prod') test.skip();
 
     await setupClerkTestingToken({ page });
 
-    await page.goto("/");
+    await page.goto('/');
 
     await clerk.signIn({
       page,
       signInParams: {
-        strategy: "password",
+        strategy: 'password',
         identifier: process.env.E2E_CLERK_USER_EMAIL,
         password: process.env.E2E_CLERK_USER_PASSWORD,
       },
@@ -72,17 +63,16 @@ test.describe("Authentication", () => {
     baseURL,
     browserName,
   }) => {
-    if (browserName === "webkit" && process.env.TEST_ENV === "prod")
-      test.skip();
+    if (browserName === 'webkit' && process.env.TEST_ENV === 'prod') test.skip();
 
     await setupClerkTestingToken({ page });
 
-    await page.goto("/");
+    await page.goto('/');
 
     await clerk.signIn({
       page,
       signInParams: {
-        strategy: "password",
+        strategy: 'password',
         identifier: process.env.E2E_CLERK_USER_EMAIL,
         password: process.env.E2E_CLERK_USER_PASSWORD,
       },
@@ -91,6 +81,33 @@ test.describe("Authentication", () => {
     await page.waitForURL(`${baseURL}/dashboard`, { timeout: 20_000 });
 
     await clerk.signOut({ page });
+
+    await page.waitForURL(`${baseURL}/**`, { timeout: 20_000 });
+    await expect(page).toHaveURL(`${baseURL}/`, { timeout: 30_000 });
+  });
+
+  test('should redirect to "/" after signing out (email and password)', async ({
+    page,
+    baseURL,
+    browserName,
+  }) => {
+    if (browserName === 'webkit' && process.env.TEST_ENV === 'prod') test.skip();
+
+    await setupClerkTestingToken({ page });
+
+    await page.goto('/');
+
+    await page.click("button:has-text('Go to Dashboard')");
+
+    await page.fill('input#identifier-field', process.env.E2E_CLERK_USER_EMAIL);
+    await page.click('button:has-text("Continue")');
+    await page.fill('input#password-field', process.env.E2E_CLERK_USER_PASSWORD);
+    await page.click('button:has-text("Continue")');
+
+    await page.waitForURL(`${baseURL}/dashboard`, { timeout: 20_000 });
+
+    await page.click('button.cl-userButtonTrigger');
+    await page.click('button:has-text("Sign out")');
 
     await page.waitForURL(`${baseURL}/**`, { timeout: 20_000 });
     await expect(page).toHaveURL(`${baseURL}/`, { timeout: 30_000 });
