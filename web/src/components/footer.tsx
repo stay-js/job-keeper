684dec0cd4091e077d7a561d57689c909f5fef97
Nagy Zétény <nagyzeteny6@gmail.com>
2025-10-15 12:38:43 +0200

    update footer

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => (
     <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
       <div className="max-md:text-center">
         &copy; 2025{' '}
-        <Link href="/" className="text-sm transition-colors hover:text-gray-200">
+        <Link href="/" className="transition-colors hover:text-gray-200">
           JobKeeper
         </Link>
         . All rights reserved.
@@ -16,7 +16,7 @@ export const Footer: React.FC = () => (
           href="https://znagy.hu"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-sm transition-colors hover:text-gray-200"
+          className="transition-colors hover:text-gray-200"
         >
           znagy.hu
         </Link>
@@ -25,12 +25,12 @@ export const Footer: React.FC = () => (
           href="https://github.com/stay-js/job-keeper"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-sm transition-colors hover:text-gray-200"
+          className="transition-colors hover:text-gray-200"
         >
           GitHub
         </Link>
 
-        <Link href="/privacy-policy" className="text-sm transition-colors hover:text-gray-200">
+        <Link href="/privacy-policy" className="transition-colors hover:text-gray-200">
           Privacy Policy
         </Link>
       </div>
